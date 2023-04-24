8bf8b2cd605e17e9b7bb8da61fbdb8b40e5c7a5b
KDavis006 <110576696+KDavis006@users.noreply.github.com>
2023-04-24 16:00:10 -0700

    made more cursor edits

---
+++ cursor.js
@@ -0,0 +1,57 @@
+// acts as the cursor
+let cursorPacman = document.getElementById('pacman');
+const onMouseMove = (e) =>{
+  cursorPacman.style.left = e.pageX + 'px';
+  cursorPacman.style.top = e.pageY + 'px';
+}
+
+document.addEventListener('mousemove', onMouseMove);
+
+// start of the ghost movement
+// this makes the ghost follow the cursor in a more delayed action so that it looks like the pacman cursor is running away from the ghost
+
+// variables for the mouses x and y cordinates
+var $mouseX = 0, $mouseY = 0;
+var $xp = 0, $yp =0;
+
+// grabs the mouse coordinates when the mouse moves
+$(document).mousemove(function(e){
+    $mouseX = e.pageX;
+    $mouseY = e.pageY;    
+});
+
+function ghostMovement(){
+
+  // check if the element has class hover, if yes then run to top left corner
+  if ($("#ghost").hasClass("hover")) {
+    $xp -= ($xp/12);
+    $yp -= ($yp/12);
+  } else { // otherwise chase the cursor
+    $xp += (($mouseX - $xp)/12);
+    $yp += (($mouseY - $yp)/12);
+  }
+  
+  // set the new position of the ghost
+  $("#ghost").css({left:$xp +'px', top:$yp +'px'});  
+}
+
+// makes it follow the mouse but slower so it looks like the cursor is running away
+var $loop = setInterval(ghostMovement, 30);
+
+// whenever you hover over anything the ghost runs away from your cursor
+$('.hover').mouseenter(function() {
+  
+  // remove the mousemove event listener and run to top left corner
+  $(document).off("mousemove");
+  $("#ghost").addClass("hover");
+  
+}).mouseleave(function(){
+
+  // add back the mousemove event listener and chase the cursor
+  $(document).on("mousemove", function(e) {
+    $mouseX = e.pageX;
+    $mouseY = e.pageY;
+  });
+  $("#ghost").removeClass("hover");
+  
+});
